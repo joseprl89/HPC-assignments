09adbce64db98b91a0d2900d5887ebcc25098632
Josep Rodríguez <joseprl89@gmail.com>
2013-12-21 19:27:32 +0100

    Implemented ping-pong measurement.

--- src/hello.c
+++ src/hello.c
@@ -6,71 +6,89 @@
 
 #define MICROS_IN_SECOND 1000000
 
-// Method based on http://stackoverflow.com/questions/1468596/c-programming-calculate-elapsed-time-in-milliseconds-unix
-// To calculate ellapsed time in milliseconds.
-
-/* Return 1 if the difference is negative, otherwise 0.  */
-int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1) {
-    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
-    result->tv_sec = diff / 1000000;
-    result->tv_usec = diff % 1000000;
-
-    return (diff<0);
-}
-
-long millisDifference(struct timeval *tv, struct timeval *tv2) {
-	long start = (tv->tv_sec * MICROS_IN_SECOND + tv->tv_usec);
-	long end = (tv2->tv_sec * MICROS_IN_SECOND + tv2->tv_usec);
-	return start - end;
-}
-
-/**
- * Main method
- */
-int main(int argc, char** argv) {
-	struct timeval tStartup, tInit, tPing, tPong, tFinish;
-	gettimeofday(&tInit, NULL);
-	
-	int MyProc, tag=1;
-	char msg='A', msg_recpt;
-	MPI_Status status;
-
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &MyProc);
-	printf("Process # %d started \n", MyProc);
-	
-	MPI_Barrier(MPI_COMM_WORLD);
-	gettimeofday(&tStartup, NULL);
-	
-	if (MyProc == 0) {
-		gettimeofday(&tPing, NULL);
-		printf("Sending message to Proc #1 \n") ;
-		MPI_Send(&msg, 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD);
-
-		MPI_Recv(&msg_recpt, 1, MPI_CHAR, 1, tag, MPI_COMM_WORLD, &status);
-		printf("Recv'd message from Proc #1 \n") ;
-		gettimeofday(&tPong, NULL);
-	}
-	else {
-		MPI_Recv(&msg_recpt, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
-		printf("Recv'd message from Proc #0 \n") ;
-
-		printf("Sending message to Proc #0 \n") ;
-		MPI_Send(&msg, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD);
+	/**
+	 * get microdifference between two timevals.
+	 */
+	long microsDifference(struct timeval *tv, struct timeval *tv2) {
+		long start = (tv->tv_sec * MICROS_IN_SECOND + tv->tv_usec);
+		long end = (tv2->tv_sec * MICROS_IN_SECOND + tv2->tv_usec);
+		return start - end;
 	}
 
-	printf("Finishing proc %d\n", MyProc); 
+	/**
+	 * Sends pings back and forth accross the nodes.
+	 */
+	int pingData (int dataSize, int repetitions, int tag, int id) {
+		// Status
+		MPI_Status status;
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	MPI_Finalize();
-	gettimeofday(&tFinish, NULL);
-	
-	
-	
-	if ( MyProc == 0 ) {
-		printf(">>>>>>>>>>>>>> Initialized in %lu micros.\n", millisDifference(&tStartup, &tInit));
-		printf(">>>>>>>>>>>>>> Started pinging in %lu micros.\n", millisDifference(&tPing, &tStartup));
-		printf(">>>>>>>>>>>>>> Finished ping-pong in %lu micros.\n", millisDifference(&tPong, &tPong));
-		printf(">>>>>>>>>>>>>> Finished total execution in %lu micros.\n", millisDifference(&tFinish, &tInit));
+		char* sent;
+		char* received;
+		sent = (char*) malloc(dataSize * sizeof(char));
+		received = (char*) malloc(dataSize * sizeof(char));
+
+		// Repeat as many times as required.
+		for ( int i = 0 ; i < repetitions ; i++ ) {
+			// If my id is number 0, send ping then receive pong.
+			if (id == 0) {
+				MPI_Send(sent, dataSize * sizeof(char), MPI_CHAR, 1, tag, MPI_COMM_WORLD);
+				MPI_Recv(received, dataSize * sizeof(char), MPI_CHAR, 1, tag, MPI_COMM_WORLD, &status);
+			}
+			// If it is 1, pong then ping.
+			else {
+				MPI_Recv(received, dataSize * sizeof(char), MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
+				MPI_Send(sent, dataSize * sizeof(char), MPI_CHAR, 0, tag, MPI_COMM_WORLD);
+			}
+		}
+
+		free(sent);
+		free(received);
 	}
-}
+
+	/**
+	 * Main method
+	 */
+	int main(int argc, char** argv) {
+		struct timeval tStartup, tInit, tFinish,tLoopStart,tLoopEnd;
+		gettimeofday(&tInit, NULL);
+	
+		int id, tag=1;		
+
+		int repetitions = 1000;
+
+		MPI_Init(&argc, &argv);
+		MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+		// CSV header
+		if ( id == 0 ) {
+			printf("ProcessId, bytesize, repetitions, elapsed\n");
+		}
+
+		// Barrier the processes before starting for.
+		MPI_Barrier(MPI_COMM_WORLD);
+		// Measure time of day.
+		gettimeofday(&tStartup, NULL);
+
+		for ( int i = 2 ; i < (2<<20) ; i = i << 1) {
+			// Measure time of day.
+			gettimeofday(&tLoopStart, NULL);
+
+			// Ping data, 100 repetitions
+			pingData (i,repetitions,tag, id);
+
+			// Barrier the processes to ensure all have ended correctly
+			MPI_Barrier(MPI_COMM_WORLD);
+
+			// Get time of day again to compare to startup.
+			gettimeofday(&tLoopEnd, NULL);
+
+			// Print result in CSV format.
+			printf("%d,%d,%d,%lu\n", id, i , repetitions, microsDifference(&tLoopEnd, &tLoopStart));
+		}
+
+		MPI_Finalize();
+		if ( id == 0 ) {
+			// To remove no csv output use grep -v NOCSV
+			printf("NOCSV >>>>> Initialized in %lu micros.\n", microsDifference(&tStartup, &tInit));
+		}
+	}
