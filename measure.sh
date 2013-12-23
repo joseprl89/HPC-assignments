dbe2fa58a9964e2a420118972ab4ae81527174fe
Josep Rodríguez <joseprl89@gmail.com>
2013-12-23 20:49:10 +0100

    Updated doc files. Prepared for delivery

--- measure.sh
+++ measure.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-
 echo "PROBLEMSIZE, ITERATIONS, PROCESSES, TIME,JOB";
-for problem_size in 10 20 50 100 200 500 1000 2000 5000;
+#Iterate over problem size
+for problem_size in 200 500 1000 2000;
 do
-	for iters in 1 10 100 1000;
-	do
-		for THREADS in 2 4 8;
-		do
-			/usr/bin/mpirun -np $THREADS bin/solver $problem_size $iters;
-		done;
-	done;
+	mpirun --hostfile mpihosts-np 8 bin/solver $problem_size 1000;
+done;
 
+# Iterate over iterations.
+for iters in 1 10 100 1000 2000;
+do
+	mpirun --hostfile mpihosts -np 8 bin/solver 1000 $iters;
+done;
+
+# Iterate over num threads.
+for THREADS in 2 4 8;
+do
+	mpirun --hostfile mpihosts -np $THREADS bin/solver 1000 1000;
 done;
